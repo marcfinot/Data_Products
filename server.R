083ed60b5c6a476b4f3e965e94be9e67c1e81cbb
marc.finot <marc.finot@gmail.com>
2015-02-21 10:55:59 -0800

    update with real application

--- server.R
+++ server.R
@@ -1,8 +1,25 @@
-diabetesRisk <- function(glucose) glucose / 200
+TMYlocationfile <- "http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/tmy3/TMY3_StationsMeta.csv"
+location_table <- read.csv(TMYlocationfile, header=TRUE)
+location_table$location <- paste(location_table$State,location_table$Site.Name)
+index <- order(location_table$location)
+location_sorted <- location_table[index,]
+
+solarRadiation <- function(location){
+  location_USAF <- location_sorted$USAF[(location == location_sorted$location)]
+#  location_USAF <- location
+  file <- paste("http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/data/tmy3/",location_USAF,"TYA.CSV",sep="")
+#  print(file)
+  table <- read.csv(file,header = TRUE, skip = 1)
+  daily_sun <- tapply(0.001 * table$GHI..W.m.2., table$Date..MM.DD.YYYY., sum)
+  return(daily_sun)
+} 
 
 shinyServer(
   function(input, output) {
-    output$inputValue <- renderPrint({input$glucose})
-    output$prediction <- renderPrint({diabetesRisk(input$glucose)})
+    output$inputValue <- renderPrint({input$locationID})
+    output$prediction <- renderPrint({summary(solarRadiation(input$locationID))})
+    output$newHist <- renderPlot({
+      hist(solarRadiation(input$locationID), xlab='daily radiation (kW/m2)', col='lightblue',main=input$location)
+    })
   }
 )
--- ui.R
+++ ui.R
@@ -1,18 +1,26 @@
 library(shiny)
+TMYlocationfile <- "http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/tmy3/TMY3_StationsMeta.csv"
+location_table <- read.csv(TMYlocationfile, header=TRUE)
+location_table$location <- paste(location_table$State,location_table$Site.Name)
+index <- order(location_table$location)
+location_sorted <- location_table[index,]
+
 shinyUI(
   pageWithSidebar(
     # Application title
-    headerPanel("Diabetes prediction"),
+    headerPanel("Solar Radiation summary based on TMY data"),
     sidebarPanel(
-      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
-      submitButton('Submit')
+#      selectInput('locationID','Please choose location',location_sorted$USAF,selected = 704540),
+      selectInput('locationID','location name',location_sorted$location,selected = "CA MOUNTAIN VIEW MOFFETT FLD NAS"),
+      submitButton('Submit'),
+      h5('data downloaded from http://rredc.nrel.gov/')
     ),
     mainPanel(
-      h3('Results of prediction'),
-      h4('You entered'),
+      h3('Results of solar radiation info for the following location'),
       verbatimTextOutput("inputValue"),
-      h4('Which resulted in a prediction of '),
-      verbatimTextOutput("prediction")
+      h4('typical daily insolation in kWh/m2 (equiv. number of hours of sun)'),
+      verbatimTextOutput("prediction"),
+      plotOutput('newHist')
     )
   )
 )
